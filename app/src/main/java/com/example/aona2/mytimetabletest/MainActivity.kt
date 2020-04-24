138918e0bf0280b1256efbd2b6245d8968304b71
yasu307 <yusuke307.ac@gmail.com>
2020-04-24 22:14:34 +0900

    linearLayoutを配列で管理する

--- app/src/main/java/com/example/aona2/mytimetabletest/MainActivity.kt
+++ app/src/main/java/com/example/aona2/mytimetabletest/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.aona2.mytimetabletest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     val lecText = Array(6, { arrayOfNulls<TextView?>(5)})
     private var periodText : Array<TextView?> = arrayOfNulls(6)
     val daysText : Array<TextView?> = arrayOfNulls(5)
+    private var LinearArray: Array<LinearLayout?> = arrayOfNulls(6)
     var textView: TextView? = null
 
     private val periodArray = arrayOf(900, 1040, 1300, 1440, 1615, 1800)
@@ -18,38 +21,43 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         setView()
+
     }
 
     private fun setView(){
         val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.0f)
         val halfParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 0.5f)
+        val linearParams = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1.0f)
+        val linearHalfParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 0.5f)
+        val linearWrapPrams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT, 0.5f)
+
+        LinearArray[0] = LinearLayout(this)
+        LinearArray[0]?.orientation = LinearLayout.VERTICAL
+        LinearArray[0]?.layoutParams = linearWrapPrams
+
+        for(i in 1..5) {
+            LinearArray[i] = LinearLayout(this)
+            LinearArray[i]?.orientation = LinearLayout.VERTICAL
+            LinearArray[i]?.layoutParams = linearParams
+        }
+
         textView = TextView(this)
         textView?.layoutParams = halfParams
-        periodLinear.addView(textView)
+        LinearArray[0]?.addView(textView)
         for(i in 0..5) {
             periodText[i] = TextView(this)
             val time = periodArray[i]
-            //periodText[i]?.text = "test"
-            periodText[i]?.text = (i+1).toString() + "限\n" + (time/10).toString() + ":" + (time%10).toString()
+            periodText[i]?.text = (i+1).toString() + "限\n" + (time/100).toString() + ":" + (time%100).toString()
             periodText[i]?.layoutParams = params
-            periodLinear.addView(periodText[i])
+            LinearArray[0]?.addView(periodText[i])
         }
 
-
-
-        /*
-        val textLayoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT, 1.0f)
-        val textView: Array<TextView?> = arrayOfNulls(7)
-        for(i in 0..6) {
-            textView[i] = TextView(this)
-            textView[i]?.text = "test ${i}"
-            textView[i]?.layoutParams = textLayoutParams
-            Timetable.addView(textView[i])
-
-
-         */
+        horizonLinear?.addView(LinearArray[0])
+        for(i in 1..5) {
+            horizonLinear?.addView(LinearArray[i])
+        }
 
 
     }
