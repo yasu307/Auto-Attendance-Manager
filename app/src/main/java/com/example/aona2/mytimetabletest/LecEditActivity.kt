030518720f39625ce3fc69d87d93a1d5ab101cd8
yasu307 <yusuke307.ac@gmail.com>
2020-04-25 12:19:08 +0900

    時間割のコマの設定、授業コマのクリック処理

---
+++ app/src/main/java/com/example/aona2/mytimetabletest/LecEditActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aona2.mytimetabletest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LecEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lec_edit)
+    }
+}
--- app/src/main/java/com/example/aona2/mytimetabletest/MainActivity.kt
+++ app/src/main/java/com/example/aona2/mytimetabletest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.aona2.mytimetabletest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -21,44 +22,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         setView()
-
     }
 
     private fun setView(){
+        //レイアウトパラメータの宣言
         val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 1.0f)
         val halfParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0, 0.5f)
         val linearParams = LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.MATCH_PARENT, 1.0f)
         val linearHalfParams = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 0.5f)
         val linearWrapPrams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.MATCH_PARENT, 0.5f)
 
+        //LinearLayoutの設定
         LinearArray[0] = LinearLayout(this)
         LinearArray[0]?.orientation = LinearLayout.VERTICAL
-        LinearArray[0]?.layoutParams = linearWrapPrams
-
+        LinearArray[0]?.layoutParams = linearHalfParams
+        horizonLinear?.addView(LinearArray[0])
         for(i in 1..5) {
             LinearArray[i] = LinearLayout(this)
             LinearArray[i]?.orientation = LinearLayout.VERTICAL
             LinearArray[i]?.layoutParams = linearParams
+            horizonLinear?.addView(LinearArray[i])
         }
 
+        //1列目：時限と時間の表示
         textView = TextView(this)
         textView?.layoutParams = halfParams
         LinearArray[0]?.addView(textView)
         for(i in 0..5) {
             periodText[i] = TextView(this)
             val time = periodArray[i]
-            periodText[i]?.text = (i+1).toString() + "限\n" + (time/100).toString() + ":" + (time%100).toString()
+            periodText[i]?.text = (i+1).toString() + "限\n" + (time/100).toString() + "時\n" + (time%100).toString() + "分"
             periodText[i]?.layoutParams = params
             LinearArray[0]?.addView(periodText[i])
         }
 
-        horizonLinear?.addView(LinearArray[0])
-        for(i in 1..5) {
-            horizonLinear?.addView(LinearArray[i])
+        //1行目:曜日の表示
+        val days = resources?.getStringArray(R.array.Days)
+        for(i in 0..4){
+            daysText[i] = TextView(this)
+            if(days != null) daysText[i]?.text = days[i]
+            daysText[i]?.layoutParams = halfParams
+            LinearArray[i+1]?.addView(daysText[i])
         }
 
-
+        //授業コマの表示
+        for(i in 0..4){
+            for(j in 0..5){
+                lecText[j][i] = TextView(this)
+                lecText[j][i]?.text = j.toString() + i.toString()
+                lecText[j][i]?.layoutParams = params
+                LinearArray[i+1]?.addView(lecText[j][i])
+                //クリックの処理
+                lecText[j][i]?.setOnClickListener{
+                    val intent = Intent(it.context, LecEditActivity::class.java)
+                    intent.putExtra("period", j.toString())
+                    intent.putExtra("day", i.toString())
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
