d78d0c4f3bc78e8d025e527c63e52cd82e608396
yasu307 <yusuke307.ac@gmail.com>
2020-04-26 12:14:03 +0900

    periodActivityの作成

---
+++ app/src/main/java/com/example/aona2/mytimetabletest/PeriodActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aona2.mytimetabletest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PeriodActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_period)
+    }
+}
