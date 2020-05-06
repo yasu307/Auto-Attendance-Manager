51f1eb9a1f42e1e71d29fd795e00255bed70778c
yasu307 <yusuke307.ac@gmail.com>
2020-05-06 22:09:50 +0900

    改行

--- app/src/main/java/com/example/aona2/mytimetabletest/TimePickerFragment.kt
+++ app/src/main/java/com/example/aona2/mytimetabletest/TimePickerFragment.kt
@@ -1,6 +1,5 @@
 package com.example.aona2.mytimetabletest
 
-import android.app.Activity
 import android.app.Dialog
 import android.app.TimePickerDialog
 import android.os.Bundle
@@ -8,11 +7,13 @@ import android.text.format.DateFormat
 import androidx.fragment.app.DialogFragment
 
 
-class TimePickerFragment(private var listener: TimePickerDialog.OnTimeSetListener, private val preference: Preference, private val index:Int): DialogFragment(){
+class TimePickerFragment(private var listener: TimePickerDialog.OnTimeSetListener,
+                         private val preference: Preference,
+                         private val index:Int): DialogFragment(){
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        // Create a new instance of TimePickerDialog and return it
         val time = preference.periodArray[index]
-        return TimePickerDialog(activity, R.style.TimePickerDialogTheme, listener, time/100, time%100, DateFormat.is24HourFormat(activity))
+        return TimePickerDialog(activity, R.style.TimePickerDialogTheme, listener,
+            time/100, time%100, DateFormat.is24HourFormat(activity))
     }
 }
